6db7305c8b1019abede748fd45d2f2ce552b0ef3
k32 <10274441+k32@users.noreply.github.com>
2019-05-25 12:08:43 +0200

    Add filtering by metatype in lee_consult

--- src/application/lee_consult.erl
+++ src/application/lee_consult.erl
@@ -2,12 +2,18 @@
 
 -export([ metamodel/0
         , read/2
+        , read/3
         , read_to/3
+        , read_to/4
         ]).
+
+-export_type([filter/0]).
 
 -include("lee.hrl").
 
 -define(consult, consult).
+
+-type filter() :: [lee:metatype()] | all.
 
 -spec metamodel() -> lee:module().
 metamodel() ->
@@ -21,13 +27,35 @@ metamodel() ->
 -spec read_to(lee:model(), file:filename(), lee_storage:data()) ->
                      lee_storage:data().
 read_to(Model, Filename, Data) ->
-    Patch = read(Model, Filename),
+    read_to(Model, Filename, all, Data).
+
+%% @doc Parse file into a `lee_storage'
+%% @throws {error, string()}
+-spec read_to(lee:model(), file:filename(), filter(), lee_storage:data()) ->
+                     lee_storage:data().
+read_to(Model, Filename, Filter, Data) ->
+    Patch = read(Model, Filename, Filter),
     lee_storage:patch(Data, Patch).
 
 %% @doc Parse file into a patch
 %% @throws {error, string()}
 -spec read(lee:model(), file:filename()) -> lee:patch().
 read(Model, Filename) ->
+    read(Model, Filename, all).
+
+%% @doc Parse file into a patch
+%% @throws {error, string()}
+-spec read(lee:model(), file:filename(), filter()) -> lee:patch().
+read(Model, Filename, Filter) ->
+    Predicate = case Filter of
+                    all ->
+                        fun(_) -> true end;
+                    _ ->
+                        OrdSet = ordsets:from_list(Filter),
+                        fun(MetaTypes) ->
+                                ordsets:intersection(OrdSet, MetaTypes) =/= []
+                        end
+                end,
     Keys = lee_model:get_metatype_index(?consult, Model),
     Terms0 = case file:consult(Filename) of
                  {ok, T0} ->
@@ -48,17 +76,17 @@ read(Model, Filename) ->
                      end
     end,
     lists:foldl( fun(Key, Acc) ->
-                         read_val(Model, Terms, Key, Acc)
+                         read_val(Model, Predicate, Terms, Key, Acc)
                  end
                , []
                , Keys).
 
-
-read_val(Model, Terms, Key, Acc) ->
-    #mnode{metaparams = Attrs} = lee_model:get(Key, Model),
+read_val(Model, Predicate, Terms, Key, Acc) ->
+    #mnode{metatypes = MT, metaparams = Attrs} = lee_model:get(Key, Model),
+    Valid = Predicate(MT),
     FileKey = maps:get(file_key, Attrs),
     case Terms of
-        #{FileKey := Val} ->
+        #{FileKey := Val} when Valid ->
             [{set, Key, Val} | Acc];
         #{} ->
             Acc
